fcf50d98ae312f732edd59eaa802867de2d50e59
davidmerkt <davidmerkt@users.noreply.github.com>
2022-06-21 21:34:45 -0400

    Include APK reference in the project

---
+++ AndroidTestAutomation/Element.cs
@@ -0,0 +1,16 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AndroidTestAutomation
+{
+    public abstract class Element
+    {
+        public abstract By By { get; }
+        public abstract string Text { get; }
+        public abstract void Click();
+    }
+}
--- AndroidTestAutomation/UnitTest1.cs
+++ AndroidTestAutomation/UnitTest1.cs
@@ -19,15 +19,17 @@ namespace AndroidTestAutomation
         {
             Console.WriteLine("Start driver");
 
+            // Create DriverOptions object, which will contain information on the APK to load for testing
             DriverOptions capabilities = new AppiumOptions();
-            capabilities.AddAdditionalCapability(MobileCapabilityType.App, $"C:/ApiDemos-debug.apk"); 
+            capabilities.AddAdditionalCapability(MobileCapabilityType.App, $"{System.AppDomain.CurrentDomain.BaseDirectory.ToString()}/assets/ApiDemos-debug.apk"); 
 
+            // Create new driver object. It will use the Appium Desktop server that we're currently running.
             driver = new AndroidDriver<AndroidElement>(new Uri("http://localhost:4723/wd/hub"), capabilities, TimeSpan.FromSeconds(180));
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
         }
 
         [ClassCleanup]
-        public static void ClassCleanup()
+        public static void Cleanup()
         {
             driver.Quit();
         }
@@ -36,8 +38,12 @@ namespace AndroidTestAutomation
         [TestMethod]
         public void TestShouldClickAButtonThatOpensAnAlertAndThenDismissesIt()
         {
+            Console.WriteLine("Start test");
+
+            // Open the App/Alert Dialogs page
             driver.StartActivity("io.appium.android.apis", ".app.AlertDialogSamples");
 
+            // Cause a dialog to be opened, and assert that the text in the dialog is correct
             AndroidElement openDialogButton = driver.FindElementById("io.appium.android.apis:id/two_buttons");
             openDialogButton.Click();
 
@@ -45,6 +51,7 @@ namespace AndroidTestAutomation
             String alertText = alertElement.Text;
             Assert.AreEqual("Lorem ipsum dolor sit aie consectetur adipiscing\r\nPlloaso mako nuto siwuf cakso dodtos anr koop.", alertText);
 
+            // Close the dialog
             driver.FindElementById("android:id/button1").Click();
         }
     }
