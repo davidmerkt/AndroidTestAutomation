9a4d0bbc58c25f6f00b142ed2df3f208a3aaa62a
davidmerkt <davidmerkt@users.noreply.github.com>
2022-06-23 08:24:13 -0400

    Create ApiDemos page

---
+++ AndroidTestAutomation/Pages/ApiDemos.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium.Appium.Android;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AndroidTestAutomation
+{
+    public class ApiDemos
+    {
+        private readonly AndroidDriver<AndroidElement> _driver;
+
+        public ApiDemos(AndroidDriver<AndroidElement> driver)
+        {
+            _driver = driver;
+        }
+
+        private AndroidElement Access_ibilityButton => _driver.FindElementByAccessibilityId("Access'ibility");
+        private AndroidElement AccessibilityButton => _driver.FindElementByAccessibilityId("Accessibility");
+        private AndroidElement AnimationButton => _driver.FindElementByAccessibilityId("Animation");
+        private AndroidElement AppButton => _driver.FindElementByAccessibilityId("App");
+
+        public void ClickButtonByAccessibilityId(string accessibilityId) => 
+            _driver.FindElementByAccessibilityId(accessibilityId).Click();
+    }
+}
