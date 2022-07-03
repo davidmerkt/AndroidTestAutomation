ecc957486137839522122b390e1937634609048c
davidmerkt <davidmerkt@users.noreply.github.com>
2022-07-03 19:03:30 -0400

    Organize pages

---
+++ AndroidTestAutomation/Pages/Views/Views.cs
@@ -0,0 +1,43 @@
+﻿using OpenQA.Selenium.Appium.Android;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AndroidTestAutomation
+{
+    public class Views
+    {
+        private readonly AndroidDriver<AndroidElement> _driver;
+
+        public Views(AndroidDriver<AndroidElement> driver)
+        {
+            _driver = driver;
+        }
+
+        public void Goto()
+        {
+            _driver.StartActivity("io.appium.android.apis", "ApiDemos");
+            _driver.FindElementByAccessibilityId("Views").Click();
+        }
+
+        private AndroidElement ButtonsButton => _driver.FindElementByAccessibilityId("Buttons");
+        private AndroidElement TextFieldsButton => _driver.FindElementByAccessibilityId("TextFields");
+
+        public ViewsButtons ClickButtonsButton()
+        {
+            ButtonsButton.Click();
+            return new ViewsButtons(_driver);
+        }
+
+        public ViewsTextFields ClickViewsTextFields()
+        {
+            TextFieldsButton.Click();
+            return new ViewsTextFields(_driver);
+        }
+
+        public void ClickButtonByAccessibilityId(string accessibilityId) => 
+            _driver.FindElementByAccessibilityId(accessibilityId).Click();
+    }
+}
